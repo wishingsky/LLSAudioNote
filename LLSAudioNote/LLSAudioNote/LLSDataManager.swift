6c69dc802d64346a8f31113b90db5f2be9753eba
weixiaoyun <weixiaoyun@hujiang.com>
2017-07-22 14:29:30 +0800

    add: 添加存取数据的方法

--- LLSAudioNote/LLSAudioNote/LLSDataManager.swift
+++ LLSAudioNote/LLSAudioNote/LLSDataManager.swift
@@ -6,9 +6,51 @@
 //  Copyright © 2017年 com.hujiang. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 class LLSDataManager {
-
+    static let shared = LLSDataManager()
+    
+    let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let myEntityName = "LLSAudio"
+    
+    /**
+     *  保存音频记录
+     *
+     *  @param name 音频名字
+     *  @param url  音频地址
+     */
+    func save(name: String, url: String) {
+        let audio =
+            NSEntityDescription.insertNewObject(
+                forEntityName: myEntityName, into: moc)
+                as! LLSAudio
+        
+        audio.name = name
+        audio.url = url
+        
+        do {
+            try moc.save()
+        } catch {
+            fatalError("\(error)")
+        }
+    }
+    
+    /**
+     *  读取所有音频记录
+     *
+     *  @return 音频记录
+     */
+    func read() -> [LLSAudio]? {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: myEntityName)
+        do {
+            let results =
+                try moc.fetch(request) as! [LLSAudio]
+            return results
+        } catch {
+            fatalError("\(error)")
+        }
+        return nil
+    }
 }
